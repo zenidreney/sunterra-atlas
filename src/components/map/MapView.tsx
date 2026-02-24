ebdb2b7e6c40e62539f0e68cdff87b7d4b94b592
Deniz <auo@tuta.io>
2026-02-24 18:12:07 +0100

    fixed client side map rendering

--- src/components/map/MapView.tsx
+++ src/components/map/MapView.tsx
@@ -1,22 +1,11 @@
 "use client";
 
-import { MapContainer, TileLayer } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
+import dynamic from "next/dynamic";
+
+const MapViewInner = dynamic(() => import("./MapViewInner"), {
+  ssr: false,
+});
 
 export default function MapView() {
-  return (
-    <div className="h-screen w-full">
-      <MapContainer
-        center={[40.4168, -3.7038]} // Madrid
-        zoom={6}
-        scrollWheelZoom
-        className="h-full w-full"
-      >
-        <TileLayer
-          attribution="&copy; OpenStreetMap contributors"
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-      </MapContainer>
-    </div>
-  );
+  return <MapViewInner />;
 }
---
+++ src/components/map/MapViewInner.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+export default function MapViewInner() {
+  return (
+    <div className="h-screen w-full">
+      <MapContainer
+        center={[40.4168, -3.7038]} // Madrid
+        zoom={6}
+        scrollWheelZoom
+        className="h-full w-full"
+      >
+        <TileLayer
+          attribution="&copy; OpenStreetMap contributors"
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+    </div>
+  );
+}
