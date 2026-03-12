fade590afaf1ba2ff598008339b21141797c32d8
Deniz <auo@tuta.io>
2026-03-12 15:44:30 +0100

    minor changes

--- src/app/monthly/page.tsx
+++ src/app/monthly/page.tsx
@@ -1,11 +1,5 @@
 import MonthlyAnalysisPanel from "@/components/panels/MonthlyAnalysisPanel";
 
-import { useLocationContext } from "@/context/LocationContext";
-
 export default async function Monthly() {
-  return (
-    <MonthlyAnalysisPanel />
-  )
-
+  return <MonthlyAnalysisPanel />;
 }
-
