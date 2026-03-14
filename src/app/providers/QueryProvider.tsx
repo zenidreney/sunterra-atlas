5eb1179901299963daba96e08faa0294d4a3c13c
Deniz <auo@tuta.io>
2026-03-14 15:51:45 +0100

    finished adding tanstack for server side caching.

--- src/app/providers/QueryProvider.tsx
+++ src/app/providers/QueryProvider.tsx
@@ -7,34 +7,13 @@ import {
 } from "@tanstack/react-query";
 import getReverseGeocode from "@/utils/getReverseGeocode";
 import getSolarData from "@/utils/getSolarData";
-import { useState } from "react";
 
 const queryClient = new QueryClient();
+
 function QueryProvider({ children }: { children: React.ReactNode }) {
-  const [toggle, setToggle] = useState(0); // ← ADD STATE TOGGLE
-  console.log("QueryProviderInside re-rendered! New queryClient created.");
 
   return (
-    // Provide the client to your App
-    <QueryClientProvider client={queryClient}>
-      {children}
-      {/* TEMP DEVTOOLS FOR TESTING */}
-      <div style={{ position: "fixed", bottom: 10, right: 10, zIndex: 9999 }}>
-        <button
-          type="button"
-          onClick={() => setToggle((t) => t + 1)}
-          style={{
-            padding: "8px 12px",
-            background: "#ef4444",
-            color: "white",
-            border: "none",
-            borderRadius: 4,
-          }}
-        >
-          Force Re-render #{toggle}
-        </button>
-      </div>
-    </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
 }
 
