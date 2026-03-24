5cf9fca69836f932dfd00e72ebf4aa8c7a1063ab
Deniz <auo@tuta.io>
2026-03-24 19:05:18 +0100

    added average to raw chart.

--- src/components/charts/RawTable.tsx
+++ src/components/charts/RawTable.tsx
@@ -4,20 +4,20 @@ import getSolarColor from "@/utils/getSolarColor";
 
 export default function RawTable({
   data,
-  annualRadiation
+  annualRadiation,
 }: {
   data: { month: string; solarRadiation: number }[];
-  annualRadiation: number
+  annualRadiation: number;
 }) {
-  const dataElement = data.map(({ month, solarRadiation }) => {
-    const maxRadiation = Math.max(...data.map((value) => value.solarRadiation));
+  const maxRadiation = Math.max(...data.map((value) => value.solarRadiation));
+  const averageRadiatonColor = getSolarColor(annualRadiation);
 
-    const solarColor = getSolarColor(solarRadiation);
-    console.log(solarColor);
+  const monthlyRadiationArray = data.map(({ month, solarRadiation }) => {
+    const monthlySolarColor = getSolarColor(solarRadiation);
 
     return (
       <tr key={month} className="border-t border-amber-700">
-        <td className="px-2 py-1 font-medium text-amber-900">{month}</td>
+        <td className="px-2 py-1 text-amber-900">{month}</td>
         <td className="px-2 py-1 tabular-nums text-amber-800">
           {solarRadiation.toFixed(2)}
         </td>
@@ -26,8 +26,7 @@ export default function RawTable({
             className="h-2 rounded-xl"
             style={{
               width: `${(solarRadiation / maxRadiation) * 100}%`,
-              backgroundColor: solarColor,
-
+              backgroundColor: monthlySolarColor,
             }}
           ></div>
         </td>
@@ -37,22 +36,43 @@ export default function RawTable({
 
   return (
     <section>
-    <table className="text-sm">
-      <thead>
-        <tr className="bg-amber-100 text-amber-900 align-top">
-          <th className="text-left px-2 py-1 font-semibold">Month</th>
-          <th className="text-left px-2 py-1 font-bold">
-            Solar Radiation<span className="block text-xs"> (kWh/m²/day)</span>
-          </th>
-          <th className="text-left px-2 py-1 font-bold">
-            Relative Intensity <span className="block text-xs">% of Max</span>
-          </th>
-        </tr>
-      </thead>
-      <tbody>{dataElement}</tbody>
-    </table>
-    <p className="bg-amber-100 text-amber-900 align-top my-3 ml-1">Annual Radiation: {annualRadiation.toFixed(2)} </p>
-    
+      <table className="text-sm">
+        <thead>
+          <tr className="bg-amber-100 text-amber-900 align-top">
+            <th className="text-left px-2 py-1 font-semibold">Month</th>
+            <th className="text-left px-2 py-1 font-bold">
+              Solar Radiation
+              <span className="block text-xs"> (kWh/m²/day)</span>
+            </th>
+            <th className="text-left px-2 py-1 font-bold">
+              Relative Intensity <span className="block text-xs">% of Max</span>
+            </th>
+          </tr>
+        </thead>
+        <tbody>{monthlyRadiationArray}</tbody>
+
+        <tfoot>
+          <tr key={annualRadiation} className="border-t-2 border-amber-900 bg-amber-100">
+            <td className="px-2 py-3 font-medium text-lg text-amber-900">
+              Average
+            </td>
+            <td className="px-2 py-3 tabular-nums text-amber-800 text-lg">
+              {annualRadiation.toFixed(2)}
+            </td>
+            <td className="px-2 py-1">
+              <div
+                className="h-4 rounded-xl border border-amber-500"
+                style={{
+                  width: `${(annualRadiation / maxRadiation) * 100}%`,
+                  backgroundColor: averageRadiatonColor,
+                }}
+              ></div>
+            </td>
+          </tr>
+        </tfoot>
+
+      </table>
+     
     </section>
   );
 }
