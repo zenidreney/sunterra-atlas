4b9a7c637ca791c332c496dd48b2079224f20cc4
Deniz <auo@tuta.io>
2026-03-25 18:47:17 +0100

    changed color to red to check vercel integration.

--- src/components/charts/RadarChart.tsx
+++ src/components/charts/RadarChart.tsx
@@ -56,7 +56,7 @@ export default function RadarChartComponent({
       <Radar
         name="Solar Radiation"
         dataKey="solarRadiation"
-        stroke="#92400e"
+        stroke="red"
         fill="#f59e0b"
         fillOpacity={0.6}
       />
