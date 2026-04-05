11dcae441749fceb2e7dd8fdf25feca3d38c3e56
Deniz <zenid@tuta.io>
2026-04-05 04:05:55 +0200

    style(UI): change footer attribution.

--- src/components/panels/Footer.tsx
+++ src/components/panels/Footer.tsx
@@ -1,34 +1,23 @@
 export default function Footer() {
   return (
     <footer
-      className="py-2 sm:py-5 
+      className="flex flex-col md:flex-row justify-evenly py-1 sm:py-2 text-sm 
     bg-amber-100 border-t border-amber-800 text-center"
     >
-      <p>
-        Made with
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="20"
-          height="20"
-          //   fill="red"
-          //   className="bi bi-heart-fill"
-          className="fill-red-900 inline-block mx-3"
-          viewBox="0 0 16 16"
-          aria-hidden="true"
-        >
-          <path
-            fillRule="evenodd"
-            d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"
-          />
-        </svg>
-        by{" "}
+      <div>
+        <span>Data Source: </span>
+        <a className="font-bold inline-block underline hover:text-red-900" href="https://power.larc.nasa.gov/">NASA POWER API</a>
+      </div>
+
+      <div>
+        <span>Built by: </span>
         <a
-          className="font-bold underline hover:text-red-900"
+          className="font-bold inline-block underline hover:text-red-900"
           href="https://github.com/zenidreney/"
         >
           Deniz Yener
         </a>
-      </p>
+      </div>
     </footer>
   );
 }
