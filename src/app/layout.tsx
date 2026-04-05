22a5a4e536902412a8847ea584a09d5034bdd29d
Deniz <zenid@tuta.io>
2026-04-05 04:23:50 +0200

    fix(UI): fit map and panel to be the same padding. add border to map.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,11 +25,11 @@ export default function RootLayout({
           <LocationContextProvider>
             <div className="flex flex-col min-h-screen md:h-screen bg-amber-50">
               <TopBar />
-              <main className="flex flex-col grow md:min-h-0 gap-3 p-1 md:p-4 justify-evenly md:flex-row-reverse z-50">
-                <div className="w-full md:w-5/12 px-1 md:px-4 md:overflow-y-auto rounded-xl ">
+              <main className="flex flex-col grow md:min-h-0 gap-3 p-2.5 md:p-4 justify-evenly md:flex-row-reverse z-50">
+                <div className="w-full md:w-5/12 md:px-4 md:overflow-y-auto rounded-xl ">
                   {children}
                 </div>
-                <div className="w-full h-full md:w-7/12 md:h-auto px-3 md:px-0 rounded-xl overflow-hidden shadow-lg ">
+                <div className="w-full h-full md:w-7/12 md:h-auto border border-amber-700/80 md:px-0 rounded-xl overflow-hidden shadow-lg">
                   <MapView />
                 </div>
               </main>
